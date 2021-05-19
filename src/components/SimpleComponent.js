b64f0a57dfac35979f84b158365198bab778dc97
jinook929 <jinook929@gmail.com>
2021-05-19 09:36:59 -0400

    commit tree 247557c348c118a6e967a1a5c06e9ebb558e56a4

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,24 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react'
+
+export class SimpleComponent extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       mood: "happy"
+    }
+  }
+  handleMoodClick = () => {
+    this.setState(state => (state.mood === "happy" ? {mood: "sad"} : {mood: "happy"}))
+  }
+   render() {
+    return (
+      <div className="mood" onClick={this.handleMoodClick}>
+        {this.state.mood}
+      </div>
+    )
+  }
+}
+
+export default SimpleComponent
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import "./style.css";
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={(e) => {e.target.innerText = e.target.innerText.includes("happy") ? "I am just sad" : "I am just happy"}} />
   </div>,
   document.getElementById('root')
 );
